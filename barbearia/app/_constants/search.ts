03cce9976a93672fe1ba494868377a6360fae278
miguel.junior <miguelmialpiju@gmail.com>
2024-08-07 11:40:42 -0300

    feat: add BookingItem component

---
+++ barbearia/app/_constants/search.ts
@@ -0,0 +1,31 @@
+interface QuickSearchOption {
+  imageUrl: string,
+  title: string
+}
+
+export const quickSearchOptions: QuickSearchOption[] = [
+  {
+    imageUrl: "/tesoura.svg",
+    title: "Cabelo"
+  },
+  {
+    imageUrl: "/tesoura.svg",
+    title: "Barba"
+  },
+  {
+    imageUrl: "/lamina.svg",
+    title: "Acabamento"
+  },
+  {
+    imageUrl: "/sobrancelha.svg",
+    title: "Sobrancelha"
+  },
+  {
+    imageUrl: "/hidratacao.svg",
+    title: "Hidratação"
+  },
+  {
+    imageUrl: "/masagem.svg",
+    title: "Massagem"
+  },
+]
