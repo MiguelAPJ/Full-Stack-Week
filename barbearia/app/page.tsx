2a1c73f9a5022ae9c723cd89a07da0c1a16db618
miguel.junior <miguelmialpiju@gmail.com>
2024-08-07 10:24:37 -0300

    feat: add Barbershop component

--- barbearia/app/page.tsx
+++ barbearia/app/page.tsx
@@ -6,10 +6,14 @@ import Image  from "next/image"
 import { Card, CardContent } from "./_components/ui/card";
 import { Badge } from "./_components/ui/badge"
 import { Avatar, AvatarImage } from "./_components/ui/avatar";
+import { db } from "./_lib/prisma"
+import BarbershopItem from "./_components/barbershop-item";
 
 
 
-const Home = () => {
+const Home = async () => {
+  // chamando o banco de dados
+  const barbershops = await db.barbershop.findMany({})
   return (
   <div>
     {/* header */}
@@ -34,32 +38,46 @@ const Home = () => {
 
       {/* Agendamento */}
 
-      <div>
-        <Card className="mt-6">
-          <CardContent className="flex justify-between p-0">
-            <div className="flex flex-col gap-2 py-5 pl-5">
-              <Badge className="w-fit">Confirmado</Badge>
-              <h3 className="font-semibold">Corte de Cabelo</h3>
+      <h2 className="uppercase font-bold text-xs text-gray-400 mb-3 mt-6">
+        Agendamento
+      </h2>
 
-              <div className="flex items-center">
-                  <Avatar className="h-6 w-6">
-                    <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
-                  </Avatar>
-                  <p className="text-sm">Barbearia Miguel</p>
+      <Card>
+        <CardContent className="flex justify-between p-0">
+          <div className="flex flex-col gap-2 py-5 pl-5">
+            <Badge className="w-fit">Confirmado</Badge>
+            <h3 className="font-semibold">Corte de Cabelo</h3>
 
-              </div>
+            <div className="flex items-center">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
+                </Avatar>
+                <p className="text-sm">Barbearia Miguel</p>
 
             </div>
 
-            <div className="flex flex-col items-center justify-center px-5 border-l-2 border-solid">
-              <p className="text-sm">Agosto</p>
-              <p className="text-2xl">05</p>
-              <p className="text-sm">20:00</p>
-            </div>
-          </CardContent>
+          </div>
 
-        </Card>
-      </div>
+          <div className="flex flex-col items-center justify-center px-5 border-l-2 border-solid">
+            <p className="text-sm">Agosto</p>
+            <p className="text-2xl">05</p>
+            <p className="text-sm">20:00</p>
+          </div>
+        </CardContent>
+
+      </Card>
+
+      <h2 className="uppercase font-bold text-xs text-gray-400 mb-3 mt-6">
+        Recomendados
+      </h2>
+
+      <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+        {barbershops.map((barbershop) => (
+          <BarbershopItem key={barbershop.id} barbershop={barbershop}/>
+          
+        ))}
+      </div>     
+      
 
     </div>
   </div>
